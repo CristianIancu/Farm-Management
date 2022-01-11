2ed0e9ab490930c8a6441a9794cff78e5340eeec
florinlupo <73797662+florinlupo@users.noreply.github.com>
2022-01-11 23:03:37 +0200

    Update activit.php

--- activit.php
+++ activit.php
@@ -5,7 +5,7 @@ $con = mysqli_connect("localhost","root","","farmmanagement");
 if(isset($_POST['save_radio']))
 {
     $type  = $_POST['type'];
-    $field  = $_POST['fiel	'];
+    $field  = $_POST['fiel'];
 	$field_bun = (int) $field;
     $query = "INSERT INTO activity (type,field) VALUES ('$type','$field_bun')";
     $query_run = mysqli_query($con, $query);
